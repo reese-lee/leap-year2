7c15b4dd92216601e5dc5e45babb6769ceac6bae
Reese Lee and Zsuzsanna Mangu <reese@>
2019-04-03 09:18:40 -0700

    add result section for html code and add user-interface logic to javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
+// business logic
+
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {}
     return true;
   } else {
     return false;
   }
+});
+
+
+// user interface logic
+
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapyear(year);
+
+    $(".year").text(year);
+
+    if (!result)  { // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
 });
